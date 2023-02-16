3ff5ac7ec58f80ebc53d05823f46eb5644a8b09b
CindyMisoi <crmisoi2k@gmail.com>
2023-02-16 23:23:51 +0300

    done

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,16 @@
 class Person
-  # your code here
+  # attr_accessor :name, :birthday, :hair_color, :eye_color, :height,
+  # :weight, :handed, :complexion, :t_shirt_size,
+  # :wrist_size, :glove_size, :pant_length, :pant_width
+  
+  def initialize(avi_attributes)
+    self.class.attr_accessor(:name, :birthday, :hair_color, :eye_color, :height,
+    :weight, :handed, :complexion, :t_shirt_size,
+    :wrist_size, :glove_size, :pant_length, :pant_width)
+    
+    # iterate over attributes
+    avi_attributes.each do |key,value|
+      self.send("#{key}=",value)
+    end
+  end
 end
